ec31787aeeef52e9bc4389f0a129f83e45bbced6
Lukas <lukasstrassel@googlemail.com>
2022-08-16 18:10:12 +0200

    fix: add script for forking

--- scripts/tenderly-deploy.js
+++ scripts/tenderly-deploy.js
@@ -1,14 +1,18 @@
+/**
+ * This script deploys the contract on a tenderly fork and executes it.
+ */
 import ethers from "ethers";
 import contract from "../out/ProposalPayloadAaveFreezeV1.sol/ProposalPayloadAaveFreezeV1.json" assert { type: "json" };
-import GOV_ARTIFACT from "../out/AaveGovHelpers.sol/IAaveGov.json" assert { type: "json" };
+import GOV_ARTIFACT from "../out/GovHelpers.sol/IAaveGov.json" assert { type: "json" };
 
 const GOV = "0xEC568fffba86c094cf06b22134B23074DFE2252c";
 const SHORT_EXECUTOR = "0xEE56e2B3D491590B5b31738cC34d5232F378a8D5";
 const AAVE_WHALE = "0x25F2226B597E8F9514B3F68F00f494cF4f286491";
+const TENDERLY_FORK_URL = process.env.TENDERLY_FORK_URL;
 
-const provider = new ethers.providers.JsonRpcProvider(
-  "https://rpc.tenderly.co/fork/5366cccf-8554-4891-a2ee-29b748054929"
-);
+if (!TENDERLY_FORK_URL) throw new Error("you have to set a TENDERLY_FORK_URL");
+
+const provider = new ethers.providers.StaticJsonRpcProvider(TENDERLY_FORK_URL);
 
 const factory = new ethers.ContractFactory(
   contract.abi,
@@ -38,13 +42,35 @@ await (
 
 const id = (await governance.getProposalsCount()) - 1;
 
-console.log(id);
 await provider.send("tenderly_setStorageAt", [
   GOV,
   ethers.BigNumber.from(
     ethers.utils.keccak256(
-      ethers.utils.defaultAbiCoder.encode(["uint256", "uint256"], [id, 4])
+      ethers.utils.defaultAbiCoder.encode(["uint256", "uint256"], [id, "0x4"])
     )
-  ).add(11),
-  ethers.utils.hexZeroPad(ethers.utils.parseEther(10).toHexString(), 32),
+  )
+    .add(11)
+    .toHexString(),
+  ethers.utils.hexZeroPad(ethers.utils.parseEther("5000000").toHexString(), 32),
 ]);
+
+const activeProposal = await governance.getProposalById(id);
+await provider.send("evm_increaseBlocks", [
+  ethers.BigNumber.from(activeProposal.endBlock)
+    .sub(ethers.BigNumber.from(activeProposal.startBlock))
+    .add(1)
+    .toHexString(),
+]);
+
+await governance.queue(id);
+
+const queuedProposal = await governance.getProposalById(id);
+const timestamp = (await provider.getBlock()).timestamp;
+await provider.send("evm_increaseTime", [
+  ethers.BigNumber.from(queuedProposal.executionTime)
+    .sub(timestamp)
+    .add(1)
+    .toNumber(),
+]);
+
+await governance.execute(id);
